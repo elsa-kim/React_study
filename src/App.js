2d60d1b5bd0f623d5501cb4d827d0fca3bc1ed86
elsa-kim <ksh96611@naver.com>
2023-04-01 13:58:56 +0900

    컴포넌트_반복(map)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import Say from "./Say";
 import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
 import ScrollBox from "./ScrollBox";
+import IterationSample from "./IterationSample";
 
 // App 이라는 컴포넌트 만들어 줌, function 키워드 사용 -> 함수형 컴포넌트
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>맨 밑으로</button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
